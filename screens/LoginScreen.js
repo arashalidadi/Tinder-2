a0f7bdfdc3f745c77cfd25ff715c1978d181a7ec
Arash@m <alidadiarash71@gmail.com>
2023-06-18 23:59:46 +0430

    create-Login-UI

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,36 +1,45 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import useAuth from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
   const { signInWithGoogle, userInfo, loading } = useAuth();
+  const navigation = useNavigation();
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View className="flex-1 bg-green-400 items-center justify-around">
-      <Text>{loading ? "loading ..." : "Login to the app"}</Text>
-      <Button
-        title="Sign in with Google"
-        // disabled={!request}
-        onPress={() => signInWithGoogle()}
-      />
+    <View className="flex-1">
+      <ImageBackground
+        resizeMode="cover"
+        source={{
+          uri: "https://tinder.com/static/tinder.png",
+        }}
+        className="flex-1 "
+      >
+        <View className="flex-row justify-center absolute w-52  bg-white bottom-20 mx-20 rounded-2xl">
+          <TouchableOpacity
+            className="  p-4  "
+            onPress={() => signInWithGoogle()}
+          >
+            <Text className="text-center font-semibold ">
+              Sign in & get swiping
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 export default LoginScreen;
-
-// <>
-// <Text className="font-bold text-lg">{userInfo.name}</Text>
-// <Image
-//   source={{
-//     uri: user.picture,
-//   }}
-//   resizeMode="contain"
-//   style={{
-//     width: 70,
-//     height: 70,
-//   }}
-//   className="rounded-full  bg-green-500"
-// />
-// {console.log(userInfo)}
-// </>
